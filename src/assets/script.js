99303a4b49e6f566d3dd07ca7423bc6fd8f4d4a0
tijmen <tijmenbruggeman@gmail.com>
2024-08-07 11:23:17 +0200

    Refactor services

--- src/assets/script.js
+++ src/assets/script.js
@@ -18,4 +18,67 @@ function closeNav() {
 
     const closebtn = document.getElementById("js-navclosebtn");
     closebtn.onclick = closeNav;
+
+    initCarousel();
 })();
+
+/**
+ * Embla Carousel
+ * https://www.embla-carousel.com/
+ */
+function initCarousel() {
+    const mainCarousel = document.getElementById("js-embla-carousel-main");
+    const thumbCarousel = document.getElementById("js-embla-carousel-thumbs");
+    const emblaApiMain = EmblaCarousel(mainCarousel, {
+        startIndex: 2,
+    });
+    const emblaApiThumb = EmblaCarousel(thumbCarousel, {
+        containScroll: "keepSnaps",
+        dragFree: true,
+        startIndex: 2,
+    });
+    carouselClickHandlers(emblaApiMain, emblaApiThumb);
+    carouselThumbsBtnsActive(emblaApiMain, emblaApiThumb);
+}
+
+function carouselClickHandlers(emblaApiMain, emblaApiThumb) {
+    const slidesThumbs = emblaApiThumb.slideNodes();
+
+    const scrollToIndex = slidesThumbs.map(
+        (_, index) => () => emblaApiMain.scrollTo(index)
+    );
+
+    slidesThumbs.forEach((slideNode, index) => {
+        slideNode.addEventListener("click", scrollToIndex[index], false);
+    });
+
+    return () => {
+        slidesThumbs.forEach((slideNode, index) => {
+            slideNode.removeEventListener("click", scrollToIndex[index], false);
+        });
+    };
+}
+
+function carouselThumbsBtnsActive(emblaApiMain, emblaApiThumb) {
+    const slidesThumbs = emblaApiThumb.slideNodes();
+
+    const toggleThumbBtnsState = () => {
+        emblaApiThumb.scrollTo(emblaApiMain.selectedScrollSnap());
+        const previous = emblaApiMain.previousScrollSnap();
+        const selected = emblaApiMain.selectedScrollSnap();
+        slidesThumbs[previous].classList.remove(
+            "servicesthumbs__item--active"
+        );
+        slidesThumbs[selected].classList.add("servicesthumbs__item--active");
+    };
+
+    emblaApiMain.on("select", toggleThumbBtnsState);
+    emblaApiThumb.on("init", toggleThumbBtnsState);
+
+    return () => {
+        const selected = emblaApiMain.selectedScrollSnap();
+        slidesThumbs[selected].classList.remove(
+            "servicesthumbs__item--active"
+        );
+    };
+}
